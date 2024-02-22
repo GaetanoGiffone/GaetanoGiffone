af14c11ad706c4dfa83b8ae046499003c715e8d3
GaetanoGiffone <159489243+GaetanoGiffone@users.noreply.github.com>
2024-02-22 15:36:44 -0500

    Project 1

--- NBTestGamePackageV1.java
+++ NBTestGamePackageV1.java
@@ -1,107 +1,54 @@
-//Gaetano Giffone
+//Gaetano Giffone's Project 1
 package nb.test.gamepackagev1;
 import edu.sjcny.gpv1.*;
 import java.awt.*;
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class NBTestGamePackageV1 extends DrawableAdapter
 {  // start class 
     static NBTestGamePackageV1 ge = new NBTestGamePackageV1(); 
-    static GameBoard gb = new GameBoard(ge, "Guy's Great Game!!!");
-    static Snowpeople frothy = new Snowpeople (100,100);
+    static GameBoard gb = new GameBoard(ge, "Guy's Amazing Not Tilted Octagon");
+    static Scanner kb = new Scanner(System.in);
+    static DecimalFormat df = new DecimalFormat("##.000"); 
     
     public static void main(String[] args) 
     { // start main    
           showGameBoard(gb);
-          
+          //promt the user
+        System.out.println("Enter the length of the sides");
+        //the number for person to enter side length
+    int side = kb.nextInt();
+    //The area formula
+    double area = 2 * (1 + Math.sqrt(2)) * Math.pow(side, 2); 
+    System.out.println("The Area of the octagon is " + df.format(area));
     }   //end main
     public void draw(Graphics g)
     {// start draw
-        frothy.show(g);
-        
+        //extra color for fun
+        Graphics2D g2d = (Graphics2D)g;
+        Color color1 = Color.GREEN;
+        Color color2 = Color.BLACK;
+        GradientPaint gp = new GradientPaint(0,100,color1,0,300,color2);
+        g2d.setPaint(gp);
+        g2d.fillRect(0, 0, 500, 500);
+                //the Polygon connect the dots game
+    Polygon octa = new Polygon();
+octa.addPoint(200,150); 
+octa.addPoint(300,200);
+octa.addPoint(350,300);  
+octa.addPoint(300,400);  
+octa.addPoint(200, 450);  
+octa.addPoint(100,400);  
+octa.addPoint(70,300);  
+octa.addPoint(100,200);  
+//some extra color 
+g.setColor(Color.BLUE);
+g.fillPolygon(octa); 
+     
+    
+    
     }// end draw
     
-    ////movimento con il Button
-    public void rightButton()
-    {//start rightbutton
-        moveRight(frothy);
-    }//end right button
-    
-     public void leftButton()
-    {//start leftbutton
-        moveLeft(frothy);
-    }//end leftbutton
-     
-    public void upButton()
-    {//start rightbutton
-        moveUP(frothy);
-    }//end upbutton
-      
-     public void downButton()
-    {//start downbutton
-        moveDown(frothy);
-    }//end downbutton
-     
-    
-    public void moveRight(Snowpeople aSnowman)
-    {//start moveright
-        int currentX;
-        currentX=aSnowman.getX();
-        currentX = currentX + 10;
-        aSnowman.setX(currentX);
-    }//end moveright
-    
-    public void moveLeft(Snowpeople aSnowman)
-    {//start moveleft
-        int currentX;
-        currentX=aSnowman.getX();
-        currentX = currentX - 10;
-        aSnowman.setX(currentX);
-    }//end moveleft
-    
-    public void moveUP(Snowpeople aSnowman)
-    {//start moveup
-        int currentY;
-        currentY=aSnowman.getY();
-        currentY = currentY - 10;
-        aSnowman.setY(currentY);
-    }//end moveup
-    
-    public void moveDown(Snowpeople aSnowman)
-    {//start movedown
-        int currentY;
-        currentY=aSnowman.getY();
-        currentY = currentY + 10;
-        aSnowman.setY(currentY);
-    }//end movedown
-    
-    ////movimento con il chiave
-
-    public void keyStruck(char key)
-    {//start keystruck
-        switch (key)
-        {//start switch
-            case 'D': case 'd':
-            {
-                moveRight(frothy);
-                break;
-            }
-            case 'A': case 'a':
-            {
-                moveLeft(frothy);
-                break;
-            }
-            case 'W': case 'w':
-            {
-                moveUP(frothy);
-                break;
-            }
-            case 'S': case 's':
-            {
-                moveDown(frothy);
-                break;
-            }
-            
-        }//end switch
-    }//end key struck
 }// end class
  
